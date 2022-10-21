2b95f6368be46dc967451ffc96913198b19545b0
GEO X TEN <46945867+GeoXTen@users.noreply.github.com>
2022-10-21 13:41:40 +0800

    Update config.js
    Signed-off-by: GEO X TEN  <46945867+GeoXTen@users.noreply.github.com>

--- src/config.js
+++ src/config.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 
 module.exports = {
     token: process.env.TOKEN || "",  // your bot token
-    clientID: process.env.CLIENT_ID || "", // your bot client id
-    prefix: process.env.PREFIX || "!", // bot prefix
-    ownerID: process.env.OWNERID || "", //your discord id
-    SpotifyID: process.env.SPOTIFYID || "",
-    SpotifySecret: process.env.SPOTIFYSECRET || "",
-    mongourl: process.env.MONGO_URI || "", // MongoDb URL
+    clientID: process.env.CLIENT_ID || "911854373196423208", // your bot client id
+    prefix: process.env.PREFIX || "-", // bot prefix
+    ownerID: process.env.OWNERID || "572614372812390410", //your discord id
+    SpotifyID: process.env.SPOTIFYID || "e311e16239d2491fb6c91f90facb0371",
+    SpotifySecret: process.env.SPOTIFYSECRET || "cb3f5ae4c54542d09528c07318ba8060",
+    mongourl: process.env.MONGO_URI || "mongodb+srv://bruh:bruh@cluster0.zjqq1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // MongoDb URL
     embedColor: process.env.COlOR || 0x303236, // embed colour
-    logs: process.env.LOGS || "channel_id", // channel id for guild create and delete logs
-    errorLogsChannel: process.env.errLogs || "channel_id", //error logs channel id
+    logs: process.env.LOGS || "847747001537527813", // channel id for guild create and delete logs
+    errorLogsChannel: process.env.errLogs || "847747001537527813", //error logs channel id
     SearchPlatform: process.env.searchPlatform || "youtube music", // Sets the Search Platform. Possibilities: youtube || youtube music || soundcloud
     links: {
         img: process.env.IMG || 'https://media.discordapp.net/attachments/963097935820750878/983300268131225651/20220606_145403.png', //setup system background image 
@@ -19,10 +19,10 @@ module.exports = {
     },
     nodes: [
         {
-            host: process.env.NODE_HOST || "54.37.6.86",
-            port: parseInt(process.env.NODE_PORT || "80"),
-            password: process.env.NODE_PASSWORD || "Blacky#9125",
-            secure: parseBoolean(process.env.NODE_SECURE || "false"),
+            host: process.env.NODE_HOST || "node1.kartadharta.xyz",
+            port: parseInt(process.env.NODE_PORT || "443"),
+            password: process.env.NODE_PASSWORD || "kdlavalink",
+            secure: parseBoolean(process.env.NODE_SECURE || "true"),
 
         }
     ],
